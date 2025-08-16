7c943c20aec83d59a3489c1d90713875ea10c4c7
hcarrer <hcarrer@gmail.com>
2025-08-16 15:42:08 -0300

    feat: typeface

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Niramit } from "next/font/google";
+
+const niramit = Niramit({
+  subsets: ["latin"],
+  weight: ["400"], // pick what you need
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={niramit.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,8 @@ const COLORS = [
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <p className="text-4xl font-extrabold mb-8">Design System Livro Livre</p>
       <div className="grid grid-cols-3 gap-4">
         {COLORS.map((color) => (
           <div key={color} className={`p-4 border ${color}`}>
