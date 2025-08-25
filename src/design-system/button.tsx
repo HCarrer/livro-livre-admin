b0166b2935ebc3c10624b1f2754962915fbb7fc8
hcarrer <hcarrer@gmail.com>
2025-08-25 09:10:02 -0300

    chore: button refactor

--- src/design-system/button.tsx
+++ src/design-system/button.tsx
@@ -1,3 +1,6 @@
+import { cn } from "@/lib/utils"
+import { cva } from "class-variance-authority"
+
 interface ButtonProps {
     label?: string
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
@@ -5,34 +8,68 @@ interface ButtonProps {
     disabled?: boolean
 }
 
-const BASE_CLASSNAME = 'px-6 py-3 min-w-[135px] h-[51px] text-f5 !leading-[16px] rounded-full font-semibold '
+const classNameVariants = cva(
+  'px-6 py-3 min-w-[135px] h-[51px] text-f5 !leading-[16px] rounded-full font-semibold transition-all duration-100 ease-in-out hover:drop-shadow-[-2px_2px_4px_#00000030]',
+  {
+    variants: {
+      variant: {
+        main:
+          'bg-power-blue text-white',
+        secondary:
+          'bg-navy-blue text-white',
+        tertiary:
+          'bg-soft-white border border-soft-lilac text-soft-lilac',
+        outline:
+          'bg-white border border-navy-blue text-navy-blue',
+        primaryOutline:
+          'bg-white border border-power-blue text-power-blue',
+      },
+      disabled: {
+        true: 'pointer-events-none',
+        false: 'cursor-pointer'
+      }
+    },
+    compoundVariants: [
+      {
+        variant: 'main',
+        disabled: true,
+        class: 'bg-disabled-blue text-soft-lilac border border-soft-lilac'
+      },
+      {
+        variant: 'secondary',
+        disabled: true,
+        class: 'bg-disabled-navy text-soft-lilac border border-soft-lilac'
+      },
+      {
+        variant: 'tertiary',
+        disabled: true,
+        class: 'bg-soft-white border border-soft-lilac text-soft-lilac'
+      },
+      {
+        variant: 'outline',
+        disabled: true,
+        class: 'bg-white border border-navy-blue text-navy-blue'
+      },
+      {
+        variant: 'primaryOutline',
+        disabled: true,
+        class: 'bg-white border border-power-blue text-power-blue'
+      }
+    ],
+    defaultVariants: {
+      variant: 'main',
+    }
+  }
+)
 
-const VARIANT_CLASSNAME = {
-  main: 'bg-power-blue text-white',
-  secondary: 'bg-navy-blue text-white',
-  tertiary: 'bg-soft-white border border-soft-lilac text-soft-lilac',
-  outline: 'bg-white border border-navy-blue text-navy-blue',
-  primaryOutline: 'bg-white border border-power-blue text-power-blue',
-}
 
-const  DISABLED_CLASSNAME = {
-  main: 'bg-disabled-blue text-soft-lilac border border-soft-lilac',
-  secondary: 'bg-disabled-navy text-soft-lilac border border-soft-lilac',
-  tertiary: 'bg-soft-white border border-soft-blue text-soft-blue',
-  outline: 'bg-white border border-disabled-navy text-disabled-navy',
-  primaryOutline: 'bg-white border border-disabled-blue text-disabled-blue',
-}
-
-const HOVER_CLASSNAME = 'transition-all duration-100 ease-in-out hover:drop-shadow-[-2px_2px_4px_#00000030]'
-
-
-const Button = ({label, onClick, variant = 'main', disabled = false, ...props }: ButtonProps) => {
+const Button = ({ label, onClick, variant = 'main', disabled = false, ...props }: ButtonProps) => {
   return (
     <button
-      className={`${BASE_CLASSNAME} ${disabled ? DISABLED_CLASSNAME[variant] : `${VARIANT_CLASSNAME[variant]} ${HOVER_CLASSNAME} cursor-pointer`}`}
+      {...props}
+      className={cn(classNameVariants({ variant, disabled }))}
       onClick={onClick}
       disabled={disabled}
-      {...props}
     >
       {label}
     </button>
