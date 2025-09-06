1060893bc952488507d632d98397fea8c47b26cd
hcarrer <hcarrer@gmail.com>
2025-09-06 10:38:16 -0300

    feat: hook form addition

--- src/pages/public/login/index.tsx
+++ src/pages/public/login/index.tsx
@@ -7,6 +7,7 @@ import Logo from "../../../../public/icons/logo.svg"
 import Background from "../../../../public/icons/background.svg"
 import Link from "next/link"
 import { RESET_PASSWORD, SIGNUP } from "@/constants/routes"
+import { useForm } from "react-hook-form"
 
 const GoogleButtonContent = () => (
 	<p className="flex gap-x-4 justify-center items-center">
@@ -17,6 +18,29 @@ const GoogleButtonContent = () => (
 
 const Login = () => {
 	const [showPassword, setShowPassword] = useState(false)
+	const {
+		formState: { isValid, errors },
+		setValue,
+		register,
+		handleSubmit
+	} = useForm({
+		defaultValues: { username: "", password: "" },
+		mode: 'all',
+    criteriaMode: 'all'
+	})
+
+	const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue('username', e.target.value, { shouldValidate: true, shouldDirty: true })
+	}
+
+	const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue('password', e.target.value, { shouldValidate: true, shouldDirty: true })
+	}
+
+	const onSubmit = (data: any) => {
+		// TODO: deixar login funcional
+		console.log(data)
+	}
 
 	return (
 		<div className="relative w-screen h-screen px-6 py-8 flex items-center justify-center">
@@ -27,18 +51,40 @@ const Login = () => {
 						Bem vindo!
 					</p>
 				</div>
-				<Input className="w-full" placeholder="Nome de usuário" type="text" icon="avatar" />		
-				<Input className="w-full" placeholder="Senha" type={showPassword ? "text" : "password"} icon={showPassword ? "openEye" : "closedEye"} onIconClick={() => setShowPassword(!showPassword)} />		
-				<Button label="Realizar login" variant="main" disabled className="w-full"/>
+				<Input
+					{...register('username', {
+						required: 'Campo obrigatório'
+					})}
+					className="w-full"
+					placeholder="Nome de usuário"
+					type="text"
+					icon="avatar"
+					onChange={handleUsernameChange}
+					errorMessage={errors.username?.message}
+				/>
+				<Input
+					{...register('password', {
+						required: 'Campo obrigatório'
+					})}
+					className="w-full"
+					placeholder="Senha"
+					type={showPassword ? "text" : "password"}
+					icon={showPassword ? "openEye" : "closedEye"}
+					onIconClick={() => setShowPassword(!showPassword)}
+					onChange={handlePasswordChange}
+					errorMessage={errors.password?.message}
+				/>
+				<Button label="Realizar login" variant="main" disabled={!isValid} type="submit" onClick={handleSubmit(onSubmit)} className="w-full"/>
 				<div className="w-full flex justify-between text-f7">
 					<Link href={RESET_PASSWORD}>
-						<button className="text-navy-blue">Esqueci minha senha</button>
+						<p className="text-navy-blue">Esqueci minha senha</p>
 					</Link>
 					<Link href={SIGNUP}>
-						<button className="text-power-blue">Quero criar uma conta</button>
+						<p className="text-power-blue">Quero criar uma conta</p>
 					</Link>
 				</div>
-				<div className="w-full h-0.5 bg-[#D9D9D9] rounded rounded-full"/>
+				<div className="w-full h-0.5 bg-[#D9D9D9] rounded rounded-full" id="separator-bar"/>
+				{/* TODO: implementar login do google */}
 				<Button label={<GoogleButtonContent />} variant="tertiary" className="w-full"/>
 			</div>
 			<Image src={Background} width={496} height={744} alt="Marca D'agua - Livro Livre" className="absolute z-1" />
