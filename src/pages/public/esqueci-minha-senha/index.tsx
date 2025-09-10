ae53e9e48052833d7377a89d588eb7254cfa1755
hcarrer <hcarrer@gmail.com>
2025-09-10 20:54:26 -0300

    chore: componentization refactor

--- src/pages/public/esqueci-minha-senha/index.tsx
+++ src/pages/public/esqueci-minha-senha/index.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image"
 import { useCallback, useMemo, useState } from "react"
 import Logo from "../../../../public/icons/logo.svg"
-import Background from "../../../../public/icons/background.svg"
 import Link from "next/link"
 import { LOGIN } from "@/constants/routes"
 import { FormProvider, useForm } from "react-hook-form"
 import { ForgotPasswordFormDefaultValues, ForgotPasswordFormProps } from "@/constants/forms/esqueci-minha-senha"
-import EmailStep from "@/constants/pages/esqueci-minha-senha/EmailStep"
-import TwoFactorStep from "@/constants/pages/esqueci-minha-senha/TwoFactorStep"
-import NewPasswordStep from "@/constants/pages/esqueci-minha-senha/NewPasswordStep"
+import EmailStep from "@/components/pages/esqueci-minha-senha/EmailStep"
+import TwoFactorStep from "@/components/pages/esqueci-minha-senha/TwoFactorStep"
+import NewPasswordStep from "@/components/pages/esqueci-minha-senha/NewPasswordStep"
+import Skeleton from "@/components/common/Skeleton"
 
 const ForgottenPassword = () => {
 	const [currentStep, setCurrentStep] = useState<"email" | "twoFactor" | "newPassword">("email")
@@ -18,35 +18,32 @@ const ForgottenPassword = () => {
 		mode: 'all',
     criteriaMode: 'all'
 	})
-
+	
 	const advanceStep = useCallback(() => {
 		if (currentStep === "email") setCurrentStep("twoFactor")
-		else if (currentStep === "twoFactor") setCurrentStep("newPassword")
+		if (currentStep === "twoFactor") setCurrentStep("newPassword")
 	}, [currentStep])
-
-	const memoContent = useMemo(() => {
-		if (currentStep === "email") return <EmailStep handleClick={advanceStep} />
-		if (currentStep === "twoFactor") return <TwoFactorStep handleClick={advanceStep} />
-		if (currentStep === "newPassword") return <NewPasswordStep />
-	}, [currentStep, advanceStep])
+	
+	const steps = useMemo(() => ({
+		email: <EmailStep handleClick={advanceStep} />,
+		twoFactor: <TwoFactorStep handleClick={advanceStep} />,
+		newPassword: <NewPasswordStep />
+	}), [advanceStep])
 
 	return (
 		<FormProvider {...methods} >
-			<div className="relative w-screen h-screen px-6 py-8 flex items-center justify-center">
-				<div className="z-2 w-full flex flex-col justify-center items-center gap-y-4">
+			<Skeleton>
 					<div className="flex justify-center gap-x-4 items-center">
 						<p className="text-f2 font-semibold flex gap-x-2 items-center">
 							<Image src={Logo} width={38} height={53} alt="Logotipo - Livro Livre" />
 							Alterar Senha
 						</p>
 					</div>
-					{memoContent}
+					{steps[currentStep]}
 					<Link href={LOGIN}>
 						<p className="text-navy-blue text-f7 w-full text-center">Voltar para o login</p>
 					</Link>
-				</div>
-				<Image src={Background} width={496} height={744} alt="Marca D'agua - Livro Livre" className="absolute z-1" />
-			</div>
+			</Skeleton>
 		</FormProvider>
 	)
 }
--- src/pages/public/login/index.tsx
+++ src/pages/public/login/index.tsx
@@ -4,11 +4,11 @@ import Image from "next/image"
 import { useState } from "react"
 import GoogleIcon from "../../../../public/icons/google.svg"
 import Logo from "../../../../public/icons/logo.svg"
-import Background from "../../../../public/icons/background.svg"
 import Link from "next/link"
 import { RESET_PASSWORD, SIGNUP } from "@/constants/routes"
 import { useForm } from "react-hook-form"
 import { LoginFormDefaultValues, LoginFormProps } from "@/constants/forms/login"
+import Skeleton from "@/components/common/Skeleton"
 
 const GoogleButtonContent = () => (
 	<p className="flex gap-x-4 justify-center items-center">
@@ -44,52 +44,49 @@ const Login = () => {
 	}
 
 	return (
-		<div className="relative w-screen h-screen px-6 py-8 flex items-center justify-center">
-			<div className="z-2 w-full flex flex-col justify-center items-center gap-y-4">
-				<div className="flex justify-center gap-x-4 items-center">
-					<p className="text-f2 font-semibold flex gap-x-2 items-center">
-						<Image src={Logo} width={38} height={53} alt="Logotipo - Livro Livre" />
-						Bem vindo!
-					</p>
-				</div>
-				<Input
-					{...register('username', {
-						required: 'Campo obrigatório'
-					})}
-					className="w-full"
-					placeholder="Nome de usuário"
-					type="text"
-					icon="avatar"
-					onChange={handleUsernameChange}
-					errorMessage={errors.username?.message}
-				/>
-				<Input
-					{...register('password', {
-						required: 'Campo obrigatório'
-					})}
-					className="w-full"
-					placeholder="Senha"
-					type={showPassword ? "text" : "password"}
-					icon={showPassword ? "openEye" : "closedEye"}
-					onIconClick={() => setShowPassword(!showPassword)}
-					onChange={handlePasswordChange}
-					errorMessage={errors.password?.message}
-				/>
-				<Button label="Realizar login" variant="main" disabled={!isValid} type="submit" onClick={handleSubmit(onSubmit)} className="w-full"/>
-				<div className="w-full flex justify-between text-f7">
-					<Link href={RESET_PASSWORD}>
-						<p className="text-navy-blue">Esqueci minha senha</p>
-					</Link>
-					<Link href={SIGNUP}>
-						<p className="text-power-blue">Quero criar uma conta</p>
-					</Link>
-				</div>
-				<div className="w-full h-0.5 bg-[#D9D9D9] rounded rounded-full" id="separator-bar"/>
-				{/* TODO: implementar login do google */}
-				<Button label={<GoogleButtonContent />} variant="tertiary" className="w-full"/>
+		<Skeleton>
+			<div className="flex justify-center gap-x-4 items-center">
+				<p className="text-f2 font-semibold flex gap-x-2 items-center">
+					<Image src={Logo} width={38} height={53} alt="Logotipo - Livro Livre" />
+					Bem vindo!
+				</p>
 			</div>
-			<Image src={Background} width={496} height={744} alt="Marca D'agua - Livro Livre" className="absolute z-1" />
-		</div>
+			<Input
+				{...register('username', {
+					required: 'Campo obrigatório'
+				})}
+				className="w-full"
+				placeholder="Nome de usuário"
+				type="text"
+				icon="avatar"
+				onChange={handleUsernameChange}
+				errorMessage={errors.username?.message}
+			/>
+			<Input
+				{...register('password', {
+					required: 'Campo obrigatório'
+				})}
+				className="w-full"
+				placeholder="Senha"
+				type={showPassword ? "text" : "password"}
+				icon={showPassword ? "openEye" : "closedEye"}
+				onIconClick={() => setShowPassword(!showPassword)}
+				onChange={handlePasswordChange}
+				errorMessage={errors.password?.message}
+			/>
+			<Button label="Realizar login" variant="main" disabled={!isValid} type="submit" onClick={handleSubmit(onSubmit)} className="w-full"/>
+			<div className="w-full flex justify-between text-f7">
+				<Link href={RESET_PASSWORD}>
+					<p className="text-navy-blue">Esqueci minha senha</p>
+				</Link>
+				<Link href={SIGNUP}>
+					<p className="text-power-blue">Quero criar uma conta</p>
+				</Link>
+			</div>
+			<div className="w-full h-0.5 bg-[#D9D9D9] rounded rounded-full" id="separator-bar"/>
+			{/* TODO: implementar login do google */}
+			<Button label={<GoogleButtonContent />} variant="tertiary" className="w-full"/>
+		</Skeleton>
 	)
 }
 
