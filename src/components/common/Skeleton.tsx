bf0d6031384e6e15432b7172f016e0cc0d9e877f
hcarrer <hcarrer@gmail.com>
2025-09-15 21:28:30 -0300

    fix: z index

--- src/components/common/Skeleton.tsx
+++ src/components/common/Skeleton.tsx
@@ -10,7 +10,7 @@ const Skeleton = ({
 }) => {
 	return (
 		<div className={`relative w-screen h-screen px-6 py-8 flex justify-center ${position === "center" ? "items-center" : "items-start"}`}>
-			<div className="z-2 w-full flex flex-col justify-center items-center gap-y-4">
+			<div className="z-10 w-full flex flex-col justify-center items-center gap-y-4">
 				{children}
 			</div>
 			<Image src={Background} width={496} height={744} alt="Marca D'agua - Livro Livre" className="absolute z-1" />
