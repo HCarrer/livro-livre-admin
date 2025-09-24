1dfb4cea24f170963a7b6828fb65327b2109d47f
hcarrer <hcarrer@gmail.com>
2025-09-23 21:35:39 -0300

    chore: progress bar width fix

--- src/components/common/UserScore.tsx
+++ src/components/common/UserScore.tsx
@@ -1,5 +1,5 @@
 import { HelpCircle } from "lucide-react";
-import { useEffect, useMemo, useState } from "react";
+import { useLayoutEffect, useMemo, useRef, useState } from "react";
 
 interface UserScoreProps {
   booksRead?: number;
@@ -7,11 +7,8 @@ interface UserScoreProps {
 }
 
 const UserScore = ({ booksRead = 0, booksToReturn = 0 }: UserScoreProps) => {
-  const [componentLoaded, setComponentLoaded] = useState(false);
-
-  useEffect(() => {
-    setComponentLoaded(true);
-  }, []);
+  const ref = useRef(null);
+  const [progressBarWidth, setProgressBarWidth] = useState("0%");
 
   // TODO: rever calculo
   const userRating = useMemo(
@@ -19,25 +16,18 @@ const UserScore = ({ booksRead = 0, booksToReturn = 0 }: UserScoreProps) => {
     [booksRead, booksToReturn],
   );
 
+  useLayoutEffect(() => {
+    console.log("ref.current", ref.current);
+    const rating = Math.floor(userRating) >= 5 ? 5 : Math.floor(userRating);
+    const fillPctg = (rating * 100) / 5;
+    setProgressBarWidth(`${fillPctg}%`);
+  }, [ref.current, userRating]);
+
   const ratingBarColor = useMemo(() => {
     if (userRating <= 2) return "#C00000"; // error-red
     if (userRating <= 4) return "#DBD826";
     return "#2F88E0"; // power-blue
   }, [userRating]);
-
-  // TODO: fix de document is not defined
-  const ratingBarWidth = useMemo(() => {
-    if (!componentLoaded) return;
-    const barElement = document.querySelector(
-      "#user_rating_bar",
-    ) as HTMLElement;
-    if (!barElement) return "0px";
-    const rating = Math.floor(userRating) >= 5 ? 5 : Math.floor(userRating);
-    const fillPctg = rating / 5;
-    return `${fillPctg * barElement.clientWidth}px`;
-  }, [componentLoaded, document, userRating]);
-
-  if (!componentLoaded) return null;
 
   return (
     <div className="w-full drop-shadow-[0px_0px_10px_#00000020] flex flex-col gap-y-2 p-5 bg-navy-blue rounded-[20px] text-white">
@@ -57,8 +47,9 @@ const UserScore = ({ booksRead = 0, booksToReturn = 0 }: UserScoreProps) => {
         <div className="relative w-full h-3" id="user_rating_bar">
           <div className="absolute z-20 top-0 left-0 w-full h-full rounded-full border border-white" />
           <div
-            className="absolute z-10 top-0 left-0 h-full rounded-full"
-            style={{ backgroundColor: ratingBarColor, width: ratingBarWidth }}
+            ref={ref}
+            className="absolute z-10 top-0 left-0 h-full rounded-full transition-all duration-200 ease-in-out"
+            style={{ backgroundColor: ratingBarColor, width: progressBarWidth }}
           />
         </div>
       </div>
