524acc030de569ac860dfe8a2c2fec8a49fea7fe
hcarrer <hcarrer@gmail.com>
2025-10-05 18:58:08 -0300

    feat: functional rent

--- src/services/rent.ts
+++ src/services/rent.ts
@@ -1,70 +1,61 @@
 import { auth, db } from "+/authentication/firebase";
 import { IBook } from "@/components/pages/home/rent-modal-steps/BookConfirmationStep";
-import { GoogleAuthProvider } from "firebase/auth/cordova";
 import {
   collection,
   doc,
-  getDoc,
   getDocs,
   query,
   setDoc,
   where,
+  serverTimestamp,
 } from "firebase/firestore";
-import { getBookByFields } from "./books";
 
 export const rentBook = async (
   book: IBook,
-): Promise<{
-  success: boolean;
-  status: number;
-}> => {
+  rentLocation: string,
+): Promise<{ success: boolean; status: number }> => {
   try {
     const user = auth.currentUser;
-    if (!user) return { success: false, status: 404 };
+    if (!user) return { success: false, status: 401 };
+
     const { title, author, publisher } = book;
+    if (!title || !author || !publisher) return { success: false, status: 400 };
+
     const bookDocRef = collection(db, "books");
     const q = query(
       bookDocRef,
-      where("title", "==", title.toLowerCase()),
-      where("author", "==", author.toLowerCase()),
-      where("publisher", "==", publisher.toLowerCase()),
+      where("title", "==", title.trim().toLowerCase()),
+      where("author", "==", author.trim().toLowerCase()),
+      where("publisher", "==", publisher.trim().toLowerCase()),
     );
     const querySnapshot = await getDocs(q);
+    if (querySnapshot.empty) return { success: false, status: 404 };
+
     const bookId = querySnapshot.docs[0].id;
-    if (!bookId) return { success: false, status: 404 };
 
     const rentCollection = collection(db, "rents");
-
-    // busca pelo aluguel
     const rentQuery = query(
       rentCollection,
       where("book", "==", bookId),
       where("user", "==", user.uid),
       where("returnedAt", "==", null),
     );
-    const rentQueryResult = (await getDocs(rentQuery)).docs;
-    // se encontrar aluguel, ve se ja foi entregue
-    if (rentQueryResult.length) {
-      const rentPending = rentQueryResult[0].exists();
-      if (rentPending) return { success: false, status: 401 };
-    }
+    const rentQueryResult = await getDocs(rentQuery);
+    if (!rentQueryResult.empty) return { success: false, status: 409 };
 
     const rentDocRef = doc(rentCollection);
     await setDoc(rentDocRef, {
       user: user.uid,
       book: bookId,
-      rentAt: new Date(),
-      rentLocation: "geolocalização 1",
+      rentAt: serverTimestamp(),
+      rentLocation,
       returnedAt: null,
       returnLocation: null,
-      // Adicione outros campos padrão conforme necessário
     });
+
     return { success: true, status: 200 };
   } catch (error) {
-    console.error("Error logging in with Google: ", error);
-    return {
-      success: false,
-      status: 500,
-    };
+    console.error("Error renting book: ", error);
+    return { success: false, status: 500 };
   }
 };
